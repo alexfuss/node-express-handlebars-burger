161187ae1f8c2c2dc2c5167ed1869ee08eb3d548
Alex Fuss <alexfuss2@gmail.com>
2020-08-20 14:36:19 -0400

    Updated server to reflect changes in mvc files

--- server.js
+++ server.js
@@ -1,22 +1,22 @@
 // Add dependencies
-const express = require("express");
-const bodyParser = require("body-parser");
-const methodOverride = require("method-override");
+var express = require("express");
+var bodyParser = require("body-parser");
+var methodOverride = require("method-override");
 
 // Setup server port
-let PORT = process.env.PORT || 3000;
+var PORT = process.env.PORT || 3000;
 
-let app = express();
+var app = express();
 
 app.use(express.static(__dirname + "/public"));
 
 app.use(bodyParser.urlencoded({ extended: false }));
 
-let exphbs = require("express-handlebars");
+var exphbs = require("express-handlebars");
 app.engine("handlebars", exphbs({ defaultLayout: "main" }));
 app.set("view engine", "handlebars");
 
-let router = require("./controllers/burger_controller.js");
+var router = require("./controllers/burger_controller.js");
 app.use("/", router);
 
 app.listen(PORT, function() {
