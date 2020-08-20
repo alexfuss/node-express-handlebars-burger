01deb7ee04f82086463b49cfa6df9c1fc1a0195c
Alex Fuss <alexfuss2@gmail.com>
2020-08-20 14:14:40 -0400

    Configuration js files set

---
+++ config/orm.js
@@ -0,0 +1,37 @@
+const connection = require("../config/connect.js");
+
+let orm = {
+    
+    selectAll: function(cb) {
+        connection.query("SELECT * FROM burgers", function(err, result) {
+            if (err) throw err;
+            cb(result);
+        });
+    },
+
+    insertBurger: function(burger_name, cb) {
+        connection.query("INSERT INTO burgers SET ?", {
+            burger_name: burger_name,
+            devoured: false,
+        },
+            function(err, result) {
+                if (err) throw err;
+                cb(result);
+            }
+        )
+    },
+
+
+    updateBurger: function(burgerID, cb) {
+        connection.query("UPDATE burgers SET ? WHERE ?", [{devoured: true}, {id: burgerID}],
+            function(err, result) {
+                if (err) throw err;
+                cb(result);
+            }
+        )
+    }
+}
+
+
+// Export
+module.exports = orm;
